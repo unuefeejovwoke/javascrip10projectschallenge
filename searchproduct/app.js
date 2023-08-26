fb17d25405da1fc720ea781042bc7cc613a554ac
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-25 19:36:07 -0700

    when the search box is cleared, the highlighting and background color are properly reset for all product names

--- searchproduct/app.js
+++ searchproduct/app.js
@@ -1,20 +1,32 @@
-const search = () =>{
-    const searchbox = document.getElementById("search-item").value.toUpperCase();
-    const storeitems = document.getElementById("product-list")
-    const product = document.querySelectorAll('.product')
-    const pname = storeitems.getElementsByTagName('h2')
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.trim().toUpperCase();
+    const storeitems = document.getElementById("product-list");
+    const product = document.querySelectorAll('.product');
+    const pname = storeitems.getElementsByTagName('h2');
 
-    for(var i =0; i <pname.length; i++ ){
+    for (var i = 0; i < pname.length; i++) {
         let match = product[i].getElementsByTagName('h2')[0];
 
-        if(match){
-            let textvalue = match.textContent || match.innerHTML
+        if (match) {
+            let textvalue = match.textContent || match.innerHTML;
 
-            if(textvalue.toUpperCase().indexOf(searchbox) > -1){
-                product[i].style.display ="";
-            } else{
-                product[i].style.display ="none";
+            // Clear previous background styles and reset text
+            match.innerHTML = textvalue;
+            match.style.backgroundColor = "initial";
+
+            if (searchbox && textvalue.toUpperCase().includes(searchbox)) {
+                product[i].style.display = "";
+
+                const startIndex = textvalue.toUpperCase().indexOf(searchbox);
+                const matchedText = textvalue.substr(startIndex, searchbox.length);
+
+                // Create a new span element to highlight the matched text
+                const highlightedText = `<span style="background-color: #ffab1b;">${matchedText}</span>`;
+                const newText = textvalue.replace(matchedText, highlightedText);
+                match.innerHTML = newText;
+            } else {
+                product[i].style.display = "none";
             }
         }
     }
-}
+};
