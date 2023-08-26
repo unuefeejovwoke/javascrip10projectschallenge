6c78175a40a40f823d7ae6a294d551012eedfeab
unuefeejovwoke <vuwill7114@gmail.com>
2023-08-26 13:16:05 -0700

    calculator

---
+++ calculator/app.js
@@ -0,0 +1,27 @@
+( function(){
+  
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    for(let btn of buttons){
+        btn.addEventListener('click', (e)=>{
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    }
+
+    equal.addEventListener('click', (e)=>{
+        if (screen.value === ''){
+            screen.value = "Please enter";
+        } else {
+            let answer = eval(screen.value);
+            screen.value = answer;
+        }
+    })
+
+    clear.addEventListener('click', (e)=>{
+        screen.value = "";
+    })
+}) ();
